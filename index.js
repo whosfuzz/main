d0ffaeae73b288da6e26916218552d5879e867d8
whosfuzz <whosfuzz29@gmail.com>
2025-05-28 09:28:41 -0600

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,9 @@ export default async ({ req, res, log, error }) => {
   else if(req.path === "/create")
   {
     const body = JSON.parse(req.body);
-    const createMessageDoc = await db.createDocument('669318d2002a5431ce91', '6695461400342d012490', ID.unique(), { folder: body.folder, message: body.message, seen: false }, [ Permission.write(Role.user(userId))]);
+    log(body);
+    //const getDiscordUserDoc = await db.getDocument('669318d2002a5431ce91', '683661c0000023c9dd0b', 
+    //const createMessageDoc = await db.createDocument('669318d2002a5431ce91', '6695461400342d012490', ID.unique(), { folder: body.folder, message: body.message, seen: false, createdBy:  }, [ Permission.write(Role.user(userId))]);
   }
   
   return res.json({ status: "complete" });
